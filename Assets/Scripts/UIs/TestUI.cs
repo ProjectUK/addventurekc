6924d5b5b25e7caee22fdaa067bf9f8c94244b8d
Giri Prahasta Putra <oberon4mine@gmail.com>
2016-10-25 22:29:05 +0700

    Added Godmode UI for testing

---
+++ Assets/Scripts/UIs/TestUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUI : MonoBehaviour {
+
+	[SerializeField] UIOGodMode _UIOGodMode;
+
+	// Use this for initialization
+	void Start () {
+		_UIOGodMode.Hide (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Utils/UIOverlay/Overlays/UIOGodMode.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIOGodMode : UIOverlay {
+
+	[SerializeField] GameSaveManager _GameSaveManager;
+
+	[SerializeField] InputField _CoinInputField;
+	[SerializeField] Button _MockupCoinButton;
+	[SerializeField] Button _ResetDataButton;
+	[SerializeField] Text _LogText;
+
+	public override void Start ()
+	{
+		base.Start ();
+
+		_CoinInputField.keyboardType = TouchScreenKeyboardType.NumberPad;
+
+		_MockupCoinButton.onClick.AddListener (() => {
+			int coin = int.Parse(_CoinInputField.text);
+
+			_GameSaveManager.SetCoins(coin);
+
+			_LogText.text = "Coin set to " + coin.ToString();
+		});
+
+		_ResetDataButton.onClick.AddListener (() => {
+			_GameSaveManager.DeleteAll();
+			_LogText.text = "Data resetted. ((GAME WILL RESTART....))";
+			StartCoroutine(DataResetRoutine());
+		});
+	}
+
+	IEnumerator DataResetRoutine() {
+		yield return IndieTime.Instance.WaitForSeconds (1);
+		SceneManager.LoadScene ("MainScene");
+	}
+
+	public void ToggleShow() {
+		if (IsHidden) {
+			Show (true);
+		} else {
+			Hide (true);
+		}
+	}
+}
